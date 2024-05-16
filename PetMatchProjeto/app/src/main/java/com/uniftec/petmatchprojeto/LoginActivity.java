59d3905afeb22ee88c8638e3a7e17b04affe2ae5
Fabio Augusto Silva Tessaro <fabio.augusto.tessaro@gmail.com>
2024-05-16 01:20:51 -0300

    projeto petmatch

---
+++ PetMatchProjeto/app/src/main/java/com/uniftec/petmatchprojeto/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.uniftec.petmatchprojeto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        Button authenticateButton = findViewById(R.id.authenticateButton);
+        authenticateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, TypeRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ PetMatchProjeto/app/src/main/java/com/uniftec/petmatchprojeto/TypeRegisterActivity.java
@@ -0,0 +1,48 @@
+package com.uniftec.petmatchprojeto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TypeRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_type_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        Button normalUserButton = findViewById(R.id.normalUserButton);
+        normalUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TypeRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button ongButton = findViewById(R.id.ongUserButton);
+        ongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TypeRegisterActivity.this, RegisterOngActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
